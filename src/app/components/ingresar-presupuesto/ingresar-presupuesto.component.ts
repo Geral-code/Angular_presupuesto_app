ae9310d5de356475e98796889af5f89f66214c0d
Geraldine <geraldine.orellana@gmail.com>
2022-11-10 22:08:46 -0300

    redireccion boton aceptar, estilos ingreso gasto

--- src/app/components/ingresar-presupuesto/ingresar-presupuesto.component.ts
+++ src/app/components/ingresar-presupuesto/ingresar-presupuesto.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { PresupuestoService } from 'src/app/services/presupuesto.service';
 
 @Component({
   selector: 'app-ingresar-presupuesto',
@@ -7,16 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IngresarPresupuestoComponent implements OnInit {
 
-  cantidad:number;
+  cantidad: number;
   cantidadIncorrecta: boolean;
 
-  constructor() { 
+  constructor(private _presupuestoService: PresupuestoService,
+              private router: Router) { 
     this.cantidad = 0;
     this.cantidadIncorrecta = false;
 
   }
 
   ngOnInit(): void {
+  }
+
+  agregar() {
+    if(this.cantidad > 0) {
+      this.cantidadIncorrecta = false;
+      this._presupuestoService.presupuesto = this.cantidad;  //vamos a traer el servicio con la variable presupuesto y la seteamos con lo que el usuario haya puesto en cantidad, la ALMACENAMOS EN EL SERVICIO ASI EL OTRO COMPONENTE PUEDE ACCEDER A ESTE VALOR
+      this._presupuestoService.restante = this.cantidad; //vamos a traer el servicio con la variable restante y la seteamos con cantidad
+      this.router.navigate(['/gastos']) // cuando hace click, lo direccionamos a gastos
+
+    } else {
+      this.cantidadIncorrecta = true;
+    }
   }
 
 }
