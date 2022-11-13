70a0efe133a0a6a7019340fffe8c7047a50fff7d
Geraldine <geraldine.orellana@gmail.com>
2022-11-12 23:48:07 -0300

    agrega presupuesto y restante en listar-gasto

--- src/app/components/gastos/listar-gastos/listar-gastos.component.ts
+++ src/app/components/gastos/listar-gastos/listar-gastos.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { PresupuestoService } from 'src/app/services/presupuesto.service';
 
 @Component({
   selector: 'app-listar-gastos',
   templateUrl: './listar-gastos.component.html',
   styleUrls: ['./listar-gastos.component.css']
 })
-export class ListarGastosComponent implements OnInit {
+export class ListarGastosComponent implements OnInit, OnDestroy {
+  subscription: Subscription;
+  presupuesto: number;
+  restante: number;
 
-  constructor() { }
+  constructor(private _presupuestoService: PresupuestoService) {
+   this.presupuesto = 0;
+   this.restante = 0;
+   
+   
+    this.subscription = this._presupuestoService.getGastos().subscribe((data: any) => {
+      console.log(data);
+    })
+   }
 
   ngOnInit(): void {
+    this.presupuesto = this._presupuestoService.presupuesto;
+    this.restante = this._presupuestoService.restante;
+
+  }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
 }
--- src/app/services/presupuesto.service.ts
+++ src/app/services/presupuesto.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
 import { GastosComponent } from '../components/gastos/gastos.component';
 
 @Injectable({
@@ -7,8 +8,7 @@ import { GastosComponent } from '../components/gastos/gastos.component';
 export class PresupuestoService {
   presupuesto: number; 
   restante: number;
-  
-
+  private gastos$ = new Subject<any>(); 
 
   constructor() {
     this.presupuesto = 0;
@@ -17,6 +17,10 @@ export class PresupuestoService {
 
    agregarGasto(gasto: any) {
     this.restante = this.restante - gasto.cantidad;
-}
+    this.gastos$.next(gasto);
 
+}
+  getGastos(): Observable<any>{
+    return this.gastos$.asObservable();
+  }
 }
